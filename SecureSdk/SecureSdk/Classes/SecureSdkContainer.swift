d7e98d7d0e4f9c20fc8e2c6076117edc3dd2ab54
Volkan Sönmez <sonmez.volkan92@gmail.com>
2023-08-30 21:11:40 +0300

    SDK created

---
+++ SecureSdk/SecureSdk/Classes/SecureSdkContainer.swift
@@ -0,0 +1,17 @@
+//
+//  SecureSdkContainer.swift
+//  SecureSdk
+//
+//  Created by Volkan Sönmez on 28.08.2023.
+//
+
+public class SecureSdkContainer {
+    
+    public init() {
+        
+    }
+    
+    public func sum(firstValue: Int, secondValue: Int) -> Int {
+        return firstValue + secondValue
+    }
+}
---
+++ output/SecureSdk.xcframework/ios-arm64_x86_64-simulator/SecureSdk.framework/Headers/SecureSdk.h
@@ -0,0 +1,18 @@
+//
+//  SecureSdk.h
+//  SecureSdk
+//
+//  Created by Volkan Sönmez on 28.08.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecureSdk.
+FOUNDATION_EXPORT double SecureSdkVersionNumber;
+
+//! Project version string for SecureSdk.
+FOUNDATION_EXPORT const unsigned char SecureSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureSdk/PublicHeader.h>
+
+
